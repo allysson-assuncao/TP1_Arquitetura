9b8ac2f4850b31e5b38431b748ff4f7d3c73ca4f
allysson-assuncao <anybody723@proton.me>
2025-04-15 20:08:43 -0300

    link

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("https://code-with-me.global.jetbrains.com/S0pUUvJZMJqhj_dODKl1FQ#p=IU&fp=6F61BCCCAD23B326AA5E07428BE976F99625F8AD149B20512337988B531BEB77&newUi=true");
     }
 }
