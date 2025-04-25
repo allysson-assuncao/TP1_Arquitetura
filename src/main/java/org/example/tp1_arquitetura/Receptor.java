d5e4bf971532a6d0cb9822c7280d471e3081a30b
allysson-assuncao <anybody723@proton.me>
2025-04-25 20:40:40 -0300

    atualizando com a correção do Saulo para streamCaracter e movendo a logica de carregar o conteúdo do arquivo

--- src/main/java/org/example/tp1_arquitetura/Receptor.java
+++ src/main/java/org/example/tp1_arquitetura/Receptor.java
@@ -51,6 +51,8 @@ public class Receptor {
             expoente--;
         }
 
+        System.out.print((char) codigoAscii);
+
         // Concatenando cada simbolo na mensagem original
         this.mensagem += (char) codigoAscii;
     }
